6b9948fd7a73843e41e4c2d70e1862730636f70a
simberaj <simbera.jan@gmail.com>
2019-12-08 15:32:53 +0100

    rename of ipf module, matrix rounders

--- pysynth/ipf.py
+++ pysynth/ipf.py
@@ -6,25 +6,53 @@ import pandas as pd
 from . import catdecat
 
 
-# class Unroller(Protocol):
-class Unroller:
-    def unroll(self, matrix: np.ndarray) -> np.ndarray:
+class MatrixRounder:
+    def round(self, matrix: np.ndarray) -> np.ndarray:
         raise NotImplementedError
 
 
-class LargestRemainderUnroller(Unroller):
-    def unroll(self, matrix: np.ndarray) -> np.ndarray:
-        raise NotImplementedError
+class LargestRemainderRounder(MatrixRounder):
+    def __init__(self, seed: Optional[int] = None):
+        pass # this is a deterministic rounder
+
+    def round(self, matrix: np.ndarray) -> np.ndarray:
+        # round down to integers, those are sure hits
+        rounded = matrix.astype(int)
+        # compute remainders to be distributed
+        remainders = matrix - rounded
+        sum_remaining = int(np.round(remainders.sum()))
+        # locate sum_remaining largest remainders
+        # ties are broken by selecting the ones with largest indices 
+        ind_add = np.argsort(
+            remainders, axis=None, kind='stable'
+        )[::-1][:sum_remaining]
+        rounded[np.unravel_index(ind_add, matrix.shape)] += 1
+        return rounded
 
 
-class RandomSamplerUnroller(Unroller):
-    def unroll(self, matrix: np.ndarray) -> np.ndarray:
-        raise NotImplementedError
+class RandomSamplingRounder(MatrixRounder):
+    def __init__(self, seed: Optional[int] = None):
+        self.seed = seed
+
+    def round(self, matrix: np.ndarray) -> np.ndarray:
+        matrix_sum = matrix.sum()
+        final_total = int(np.round(matrix_sum))
+        probs = (matrix / matrix_sum).flatten()
+        # print('PROBS', probs.sum())
+        np.random.seed(self.seed)
+        # randomly select cells to be included
+        bucket_is = np.random.choice(len(probs), size=final_total, p=probs)
+        # count the cells
+        cell_counts = np.bincount(bucket_is)
+        return np.hstack((
+            cell_counts,
+            np.zeros(matrix.size - len(cell_counts), dtype=cell_counts.dtype)
+        )).reshape(*matrix.shape)
 
 
-GENERATORS = {
-    'lrem': LargestRemainderUnroller,
-    'random': RandomSamplerUnroller,
+ROUNDERS = {
+    'lrem': LargestRemainderRounder,
+    'random': RandomSamplingRounder,
 }
 
 
@@ -46,8 +74,8 @@ class IPFSynthesizer:
         numeric variables to and from categorical ones. Can be specified as a
         single instance or per variable in a dictionary. If not given, a single
         instance with default setup will be created.
-    :param unroller: Method to use to reconstruct the dataset from
-        the synthesized IPF matrix. Use a Unroller instance or one of the
+    :param rounder: Method to use to round the IPF matrix to integer counts to
+        enable row generation. Use a MatrixRounder instance or one of the
         following strings:
 
         -   `'lrem'` uses the deterministic largest remainder method (see
@@ -64,14 +92,14 @@ class IPFSynthesizer:
     def __init__(self,
                  cond_dim: int = 2,
                  categorizer: Optional[catdecat.DataFrameDiscretizer] = None,
-                 unroller: Union[str, Unroller] = 'lrem',
+                 rounder: Union[str, MatrixRounder] = 'lrem',
                  ignore_cols: List[str] = [],
                  seed: Optional[int] = None,
                  ):
         self.cond_dim = cond_dim
-        self.unroller = (
-            UNROLLERS[unroller](seed=seed) if isinstance(unroller, str)
-            else unroller
+        self.rounder = (
+            ROUNDERS[rounder](seed=seed) if isinstance(rounder, str)
+            else rounder
         )
         self.discretizer = (
             discretizer if discretizer is not None
@@ -104,7 +132,9 @@ class IPFSynthesizer:
         matrix = self.synthed_matrix
         if n_rows is not None:
             matrix *= (n_rows / self.original_n_rows)
-        return self.dediscretize(self._map_axes(self.unroller.unroll(matrix)))
+        return self.dediscretize(self._map_axes(self._unroll(
+            self.rounder.round(matrix)
+        )))
 
     def fit_transform(self, dataframe: pd.DataFrame) -> pd.DataFrame:
         '''Fit the synthesizer and synthesize an equal-size dataframe.'''
@@ -126,19 +156,29 @@ class IPFSynthesizer:
     def _map_axes(array: np.ndarray) -> pd.DataFrame:
         # axis_values: Dict[str, Dict[int, Any]]
         raise NotImplementedError
+   
+    def _unroll(matrix: np.ndarray) -> np.ndarray:
+        raise NotImplementedError
+        # unravel the flattened indices to conform to matrix dimensions
+        # factors = tuple(np.cumprod(np.array(matrix.shape)[::-1])[:-1][::-1])
+        # dims = []
+        # for factor in factors:
+            # cur_dim = bucket_is // factor
+            # dims.append(cur_dim)
+            # bucket_is -= (cur_dim * factor)
+        # dims.append(bucket_is)
+        # return np.stack(np.unravel_index(matrix, dims).transpose()
 
     def _compute_seed_matrix(dataframe: pd.DataFrame) -> np.ndarray:
         raise NotImplementedError
-    
+
     def discretize(self, dataframe: pd.DataFrame) -> pd.DataFrame:
         '''Convert all variables to categorical using my discretizer.'''
         raise NotImplementedError
-    
+
     def dediscretize(self, dataframe: pd.DataFrame) -> pd.DataFrame:
         '''Reconstruct all non-categorical variables using my discretizer.'''
         raise NotImplementedError
-    
-    
 
 
 def ipf(seed_matrix: np.ndarray,
@@ -157,7 +197,8 @@ def ipf(seed_matrix: np.ndarray,
     '''
     matrix = seed_matrix.astype(float)
     n_dim = len(seed_matrix.shape)
-    assert n_dim == len(marginals), 'marginal dimensions do not match IPF seed'
+    if n_dim != len(marginals):
+        raise ValueError('marginal dimensions do not match IPF seed')
     total = marginals[0].sum()
     for i in range(1, len(marginals)):
         if not np.isclose(marginals[i].sum(), total):
--- test/test_ipf.py
+++ test/test_ipf.py
@@ -1,12 +1,13 @@
 import sys
 import os
+import itertools
 
 import numpy as np
 import pandas as pd
 import pytest
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-import pysynth.synth
+import pysynth.ipf
 import test_data
 
 IPF_PRECISION = 1e-10
@@ -34,22 +35,22 @@ def test_ipf_correct(shape, zero_fraction):
     for i, marginal in enumerate(marginals):
         margsum = marginal.sum()
         marginals[i] = np.array([val * 50 / margsum for val in marginal])
-    ipfed = pysynth.synth.ipf(seed_matrix, marginals, precision=IPF_PRECISION)
+    ipfed = pysynth.ipf.ipf(seed_matrix, marginals, precision=IPF_PRECISION)
     # check the shape and zeros are retained
     assert ipfed.shape == shape
     assert ((seed_matrix == 0) == (ipfed == 0)).all()
     for i, marginal in enumerate(marginals):
         ipfed_sum = ipfed.sum(axis=tuple(j for j in range(ipfed.ndim) if j != i))
         # check the marginal sums match
-        assert (abs(ipfed_sum - marginal) < (IPF_PRECISION * len(marginal))).all()
+        assert (abs(ipfed_sum - marginal) < (IPF_PRECISION * ipfed.size / len(marginal))).all()
 
 def test_ipf_dim_mismatch():
-    with pytest.raises(AssertionError):
-        pysynth.synth.ipf(np.random.rand(2,2), list(np.ones((3,2))))
+    with pytest.raises(ValueError):
+        pysynth.ipf.ipf(np.random.rand(2,2), list(np.ones((3,2))))
 
 def test_ipf_sum_mismatch():
     with pytest.raises(ValueError):
-        pysynth.synth.ipf(np.random.rand(2,2), [np.ones(2), np.full(2, 2)])
+        pysynth.ipf.ipf(np.random.rand(2,2), [np.ones(2), np.full(2, 2)])
 
 @pytest.mark.parametrize('openml_id', [31, 1461, 40536])
 def test_get_marginals(openml_id):
@@ -58,7 +59,7 @@ def test_get_marginals(openml_id):
         [col for col, dtype in df.dtypes.iteritems() if not pd.api.types.is_categorical_dtype(dtype)],
         axis=1
     )
-    margs, maps = pysynth.synth.IPFSynthesizer._get_marginals(df)
+    margs, maps = pysynth.ipf.IPFSynthesizer._get_marginals(df)
     for i, marg in enumerate(margs):
         assert np.issubdtype(marg.dtype, np.integer)
         assert len(marg) == df[df.columns[i]].nunique(dropna=False)
@@ -68,3 +69,26 @@ def test_get_marginals(openml_id):
         assert col in df.columns
         assert (maps[col].index == np.arange(len(maps[col].index))).all()
         assert frozenset(maps[col].values) == frozenset(df[col].unique())
+
+ROUNDERS = [
+    pysynth.ipf.LargestRemainderRounder(),
+    pysynth.ipf.RandomSamplingRounder(seed=1711),
+]
+
+UNROUND_MATRICES = [
+    np.array([[[2.,.5],[.5,0]],[[1.2,1],[1,.8]],[[1,.2],[1,1.8]]]),
+    np.random.rand(4,8,7) * 3,
+    np.where(np.random.rand(3,7,4,2) < .2, 0, np.random.rand(3,7,4,2) * 2),
+]
+
+@pytest.mark.parametrize('rder, mat', list(itertools.product(
+    ROUNDERS, UNROUND_MATRICES
+)))
+def test_rounders(rder, mat):
+    result = rder.round(mat)
+    assert np.issubdtype(result.dtype, np.integer)
+    assert result.sum() == int(np.round(mat.sum()))
+    assert result.min() >= 0
+    assert result[mat == 0].sum() == 0
+    # for dim_i, dim in enumerate(mat.shape):
+        # assert (result[:,dim_i] < dim).all()
